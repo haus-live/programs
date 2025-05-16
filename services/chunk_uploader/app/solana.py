ef4f60b4440db71b32ab460b02c6c66b7ad7bb2b
solmix <mihanix.eth@gmail.com>
2025-05-16 05:36:14 +0300

    fetch account info added

--- services/chunk_uploader/app/solana.py
+++ services/chunk_uploader/app/solana.py
@@ -1,4 +1,8 @@
 from solana.rpc.api import Client
+from solders.pubkey import Pubkey
+from solders.keypair import Keypair
+
+from solders.system_program import TransferParams, transfer
 
 from app.config import AppConfig
 
@@ -7,8 +11,10 @@ class Solana:
         self._config = config
         self._solana = Client(self._config.SOLANA_RPC_URL)
     
-    def read_uri_from_asset_account(self, asset_pubkey: bytes):
-        ...
+    def read_uri_from_asset_account(self, asset_pubkey: str):
+        pubkey = Pubkey.from_string(asset_pubkey)
+        account_info_json = self._solana.get_account_info_json_parsed(pubkey)
+        return account_info_json['uri']
     
-    def update_cose_asset_account_uri(self):
+    def update_core_asset_account_uri(self):
         ...
